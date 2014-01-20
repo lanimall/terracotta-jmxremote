16713619c5d95421154a3f05bb7eb757e2851a8c
Fabien Sanglier <fabiensanglier@gmail.com>
2014-01-19 22:25:22 -0500

    added more ehcache stats to gather

--- src/main/java/org/terracotta/utils/jmxclient/beans/CacheStats.java
+++ src/main/java/org/terracotta/utils/jmxclient/beans/CacheStats.java
@@ -2,15 +2,44 @@ package org.terracotta.utils.jmxclient.beans;
 
 public class CacheStats {
 	private final String cacheName;
+	private boolean enabled;
+	private boolean statsEnabled;
 	private long cacheSize;
+	private long localHeapSize;
+	private long localOffHeapSize;
+	private long localDiskSize;
 	private int cacheHitRatio;
 	private long cacheHitRate;
+	private long offHeapHitRate;
+	private long onHeapHitRate;
+	private long onDiskHitRate;
 	private long cacheMissRate;
+	private long onHeapMissRate;
+	private long offHeapMissRate;
+	private long onDiskMissRate;
 	private long cachePutRate;
+	private long evictionRate;
+	private long expirationRate;
 	
 	public CacheStats(String cacheName) {
 		super();
 		this.cacheName = cacheName;
+	}
+
+	public boolean isEnabled() {
+		return enabled;
+	}
+
+	public void setEnabled(boolean enabled) {
+		this.enabled = enabled;
+	}
+
+	public boolean isStatsEnabled() {
+		return statsEnabled;
+	}
+
+	public void setStatsEnabled(boolean statsEnabled) {
+		this.statsEnabled = statsEnabled;
 	}
 
 	public String getCacheName() {
@@ -23,6 +52,30 @@ public class CacheStats {
 
 	public void setCacheSize(long cacheSize) {
 		this.cacheSize = cacheSize;
+	}
+
+	public long getLocalHeapSize() {
+		return localHeapSize;
+	}
+
+	public void setLocalHeapSize(long localHeapSize) {
+		this.localHeapSize = localHeapSize;
+	}
+
+	public long getLocalOffHeapSize() {
+		return localOffHeapSize;
+	}
+
+	public void setLocalOffHeapSize(long localOffHeapSize) {
+		this.localOffHeapSize = localOffHeapSize;
+	}
+
+	public long getLocalDiskSize() {
+		return localDiskSize;
+	}
+
+	public void setLocalDiskSize(long localDiskSize) {
+		this.localDiskSize = localDiskSize;
 	}
 
 	public int getCacheHitRatio() {
@@ -49,11 +102,75 @@ public class CacheStats {
 		this.cacheMissRate = cacheMissRate;
 	}
 
+	public long getOnDiskMissRate() {
+		return onDiskMissRate;
+	}
+
+	public void setOnDiskMissRate(long onDiskMissRate) {
+		this.onDiskMissRate = onDiskMissRate;
+	}
+
 	public long getCachePutRate() {
 		return cachePutRate;
 	}
 
 	public void setCachePutRate(long cachePutRate) {
 		this.cachePutRate = cachePutRate;
+	}
+
+	public long getOffHeapHitRate() {
+		return offHeapHitRate;
+	}
+
+	public void setOffHeapHitRate(long offHeapHitRate) {
+		this.offHeapHitRate = offHeapHitRate;
+	}
+
+	public long getOnHeapHitRate() {
+		return onHeapHitRate;
+	}
+
+	public void setOnHeapHitRate(long onHeapHitRate) {
+		this.onHeapHitRate = onHeapHitRate;
+	}
+
+	public long getOnDiskHitRate() {
+		return onDiskHitRate;
+	}
+
+	public void setOnDiskHitRate(long onDiskHitRate) {
+		this.onDiskHitRate = onDiskHitRate;
+	}
+
+	public long getOnHeapMissRate() {
+		return onHeapMissRate;
+	}
+
+	public void setOnHeapMissRate(long onHeapMissRate) {
+		this.onHeapMissRate = onHeapMissRate;
+	}
+
+	public long getOffHeapMissRate() {
+		return offHeapMissRate;
+	}
+
+	public void setOffHeapMissRate(long offHeapMissRate) {
+		this.offHeapMissRate = offHeapMissRate;
+	}
+
+	public long getEvictionRate() {
+		return evictionRate;
+	}
+
+	public void setEvictionRate(long evictionRate) {
+		this.evictionRate = evictionRate;
+	}
+
+	public long getExpirationRate() {
+		return expirationRate;
+	}
+
+	public void setExpirationRate(long expirationRate) {
+		this.expirationRate = expirationRate;
 	}
 }
